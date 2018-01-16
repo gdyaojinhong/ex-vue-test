553ee8eeb8717706729ffafb70ceb151161e8f1d
gdyaojinhong@163.com <yao66846>
2018-01-16 15:46:30 +0800

    修改首页

--- src/components/MenuLeft.vue
+++ src/components/MenuLeft.vue
@@ -1,13 +1,12 @@
 <template>
   <el-menu
-    default-active="2"
+    default-active="1"
     class="el-menu-vertical-demo"
     @open="handleOpen"
     @close="handleClose">
-    <el-submenu index="1">
+<!--     <el-submenu index="1">
       <template slot="title">
-        <i class="el-icon-location"></i>
-        <span>导航一</span>
+        <span>首页</span>
       </template>
       <el-menu-item-group>
         <template slot="title">分组一</template>
@@ -21,14 +20,24 @@
         <template slot="title">选项4</template>
         <el-menu-item index="1-4-1">选项1</el-menu-item>
       </el-submenu>
-    </el-submenu>
-    <el-menu-item index="2">
-      <i class="el-icon-menu"></i>
-      <span slot="title">导航二</span>
+    </el-submenu> -->
+    <el-menu-item index="1">
+      <span slot="title">首页</span>
     </el-menu-item>
-    <el-menu-item index="3">
-      <i class="el-icon-setting"></i>
-      <span slot="title">导航三</span>
+    <el-menu-item index="2">
+      <span slot="title">用户列表</span>
+    </el-menu-item>
+    <el-menu-item index="2">
+      <span slot="title">信息列表</span>
+    </el-menu-item>
+    <el-menu-item index="4">
+      <span slot="title">信息管理</span>
+    </el-menu-item>
+    <el-menu-item index="5">
+      <span slot="title">文章管理</span>
+    </el-menu-item>
+    <el-menu-item index="6">
+      <span slot="title">关于</span>
     </el-menu-item>
   </el-menu>
 </template>
